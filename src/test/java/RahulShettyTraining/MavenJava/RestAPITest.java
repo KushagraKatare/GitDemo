b28855dea54de8268c0487ee5712e34676e2e4c2
kushagra <kushagrakatare16@gmail.com>
2022-09-25 10:08:17 +0530

    first commit

---
+++ src/test/java/RahulShettyTraining/MavenJava/RestAPITest.java
@@ -0,0 +1,17 @@
+package RahulShettyTraining.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postJIRA() {
+		System.out.println("postJIRA");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+
+}
